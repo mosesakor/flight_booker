610304ac89d3d49eb397d295e5e5d3d0331ce073
Moses Akor <gabrielmosesakor@gmail.com>
2020-11-12 02:13:49 +0100

    Projecto Completo

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,17 +10,18 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    
     @booking.save
-
     redirect_to booking_path(@booking)
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id, bookings_attributes: %i[name email])
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
